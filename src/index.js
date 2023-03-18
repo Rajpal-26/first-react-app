f9e659d757e66a627e914cb15c006f74c2f3862a
Rajpal-26 <rajpaltanwar2608@gmail.com>
2023-03-18 23:49:53 +0530

    First react app

--- src/index.js
+++ src/index.js
@@ -1,13 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+let currTime = new Date(2023,5,5,20);
+currTime = currTime.getHours();
+let greeting = "";
+
+let style = {};
+
+if (currTime >= 1 && currTime < 12) {
+  greeting = "Good Morning";
+  style.color = "green";
+}
+else if (currTime >= 12 && currTime < 19) {
+  greeting = "Good Afternoon";
+  style.color = "yellow";
+}
+else {
+  greeting = "Good Night";
+  style.color = "blue";
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <div className="head">
+      <h1>
+      Hello sir,
+        <span className="change" style={style}> { greeting }</span>
+      
+    </h1>
+    </div>
   </React.StrictMode>
 );
 
